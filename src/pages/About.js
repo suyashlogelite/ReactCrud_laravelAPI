86ab2550db2d7d8292611cb03214f21e3f53cccb
suyashlogelite <suyash.logelite@gmail.com>
2024-04-29 14:39:02 +0530

    Page Styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,123 @@
 function About() {
-    return (
-        <div>
-            <h1>About Page</h1>
+  return (
+    <div>
+      <section className="jumbotron text-center">
+        <div className="container">
+          <h1 className="jumbotron-heading mt-5">About Us</h1>
+          <p className="lead text-muted">
+            Something short and leading about the collection below—its contents,
+            the creator, etc. Make it short and sweet, but not too short so
+            folks don't simply skip over it entirely.
+          </p>
+        </div>
+      </section>
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img
+                  className="card-img-top"
+                  src="https://wallpapercave.com/wp/wp4385873.jpg"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    <small className="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img
+                  className="card-img-top"
+                  src="https://wallpapercave.com/wp/wp13791905.jpg"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    <small className="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img
+                  className="card-img-top"
+                  src="https://wallpapercave.com/wp/wp13791959.jpg"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    <small className="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default About;
-  
+    </div>
+  );
+}
+
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
 function Home() {
   return (
-      <div>
-          <h1>Home Page</h1>
+    <div>
+      <header className="bg-dark py-5">
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center justify-content-center">
+            <div className="col-lg-8 col-xl-7 col-xxl-6">
+              <div className="my-5 text-center text-xl-start">
+                <h1 className="display-5 fw-bolder text-white mb-2">
+                  Home Page
+                </h1>
+                <p className="lead fw-normal text-white-50 mb-4">
+                A student is a person who goes to school to learn something. Students can be children, teenagers, or adults who are going to school, but it may also be other people who are learning, such as in college or university. A younger student is often called a pupil. Usually, students will learn from a teacher or a lecturer if at university. They also do much reading. A student can also be a person studying for a specific profession.
+                </p>
+              </div>
+            </div>
+            <div className="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+              <img
+                className="img-fluid rounded-3 my-5"
+                src="https://wallpapercave.com/wp/wp9669759.jpg"
+                alt="..."
+              />
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
